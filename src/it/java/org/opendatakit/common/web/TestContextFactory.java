7189fd6bd191f6a5fa123b50d7ce9cca79484255
wbrunette <wbrunette@gmail.com>
2018-11-02 16:24:30 -0700

    begin fixing resources

--- src/it/java/org/opendatakit/common/web/TestContextFactory.java
+++ src/it/java/org/opendatakit/common/web/TestContextFactory.java
@@ -18,14 +18,10 @@ package org.opendatakit.common.web;
 
 import javax.servlet.ServletContext;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.Ignore;
 import org.opendatakit.common.persistence.Datastore;
-import org.opendatakit.common.persistence.engine.pgres.DatastoreImpl;
-import org.opendatakit.common.security.Realm;
 import org.opendatakit.common.security.User;
 import org.opendatakit.common.security.UserService;
-import org.opendatakit.common.security.spring.UserServiceImpl;
 import org.opendatakit.common.web.constants.BasicConsts;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -33,14 +29,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Ignore("not a test")
 public class TestContextFactory {
 
-//	  public static final String USER_BEAN = "user_service";
-//	  public static final String DATASTORE_BEAN = "datastore";
+	public static final String USER_BEAN = "user_service";
+	public static final String DATASTORE_BEAN = "datastore";
 
 	/**
 	 * Singleton of the application context
 	 */
-//	    private static final String APP_CONTEXT_PATH = "odk-settings.xml";
-//	    private static final ApplicationContext applicationContext = new ClassPathXmlApplicationContext(APP_CONTEXT_PATH);
+	private static final String APP_CONTEXT_PATH = "odk-settings.xml";
+	private static final ApplicationContext applicationContext = new ClassPathXmlApplicationContext(APP_CONTEXT_PATH);
 
 	@Ignore("not a test")
 	public static final class CallingContextImpl implements CallingContext {
@@ -62,109 +58,73 @@ public class TestContextFactory {
 			String secureport = System.getProperty("test.server.secure.port","8443");
 			serverUrl = "http://" + hostname + ":" + port + webApplicationBase;
 			secureServerUrl = "https://" + hostname + ":" + secureport + webApplicationBase;
-			try {
-				BasicDataSource dataSource = new BasicDataSource();
-				dataSource.setDriverClassName("org.postgresql.Driver");
-				dataSource.setUrl("jdbc:postgresql://localhost/postgres?autoDeserialize=true");
-				dataSource.setUsername("odk");
-				dataSource.setPassword("odk");
-				dataSource.setMaxIdle(10);
-				dataSource.setMinIdle(5);
-				dataSource.setMaxTotal(100);
-				dataSource.setMaxConnLifetimeMillis(590000);
-				dataSource.setMaxWaitMillis(30000);
-				dataSource.setValidationQuery("select schema_name from information_schema.schemata limit 1");
-				dataSource.setValidationQueryTimeout(1);
-				dataSource.setTestOnBorrow(true);
-				DatastoreImpl db = new DatastoreImpl();
-				db.setDataSource(dataSource);
-				db.setSchemaName("odk_sync");
-				datastore = db;
-
-				Realm realm = new Realm();
-				realm.setIsGaeEnvironment(false);
-				realm.setRealmString("opendatakit.org ODK Aggregate");
-				realm.setHostname("localhost");
-				realm.setPort(8888);
-				realm.setSecurePort(8443);
-				realm.setChannelType("REQUIRES_INSECURE_CHANNEL");
-				realm.setSecureChannelType("REQUIRES_INSECURE_CHANNEL");
-
-				UserServiceImpl us = new UserServiceImpl();
-				us.setRealm(realm);
-				userService = us;
-
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-			//	datastore = (Datastore) applicationContext.getBean(DATASTORE_BEAN);
-			//	userService = (UserService) applicationContext.getBean(USER_BEAN);
-			System.err.println("WAYLON WAS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			datastore = (Datastore) applicationContext.getBean(DATASTORE_BEAN);
+			userService = (UserService) applicationContext.getBean(USER_BEAN);
 		}
 
 		@Override
 		public Object getBean(String beanName) {
+			return applicationContext.getBean(beanName);
+		}
+
+		@Override
+		public Datastore getDatastore() {
+			return datastore;
+		}
+
+		@Override
+		public UserService getUserService() {
+			return userService;
+		}
+
+		@Override
+		public ServletContext getServletContext() {
 			return null;
 		}
 
-			@Override
-	    	public Datastore getDatastore() {
-	    		return datastore;
-	    	}
+		@Override
+		public String getWebApplicationURL() {
+			return webApplicationBase + BasicConsts.FORWARDSLASH;
+		}
 
-			@Override
-	    	public UserService getUserService() {
-	    		return userService;
-	    	}
+		@Override
+		public String getWebApplicationURL(String servletAddr) {
+			return webApplicationBase + BasicConsts.FORWARDSLASH + servletAddr;
+		}
 
-			@Override
-			public ServletContext getServletContext() {
-				return null;
-			}
+		@Override
+		public String getServerURL() {
+			return serverUrl;
+		}
 
-			@Override
-	    	public String getWebApplicationURL() {
-	    		return webApplicationBase + BasicConsts.FORWARDSLASH;
-	    	}
+		@Override
+		public String getSecureServerURL() {
+			return secureServerUrl;
+		}
 
-			@Override
-	    	public String getWebApplicationURL(String servletAddr) {
-	    		return webApplicationBase + BasicConsts.FORWARDSLASH + servletAddr;
-	    	}
+		@Override
+		public void setAsDaemon(boolean asDaemon ) {
+			this.asDaemon = asDaemon;
+		}
 
-			@Override
-	    	public String getServerURL() {
-	    		return serverUrl;
-	    	}
+		@Override
+		public boolean getAsDeamon() {
+			return asDaemon;
+		}
 
-			@Override
-			public String getSecureServerURL() {
-				return secureServerUrl;
-			}
+		@Override
+		public User getCurrentUser() {
+			return asDaemon ? userService.getDaemonAccountUser() : userService.getCurrentUser();
+		}
+	}
 
-			@Override
-	    	public void setAsDaemon(boolean asDaemon ) {
-	    		this.asDaemon = asDaemon;
-	    	}
+	/**
+	 * Private constructor
+	 */
+	private TestContextFactory() {}
 
-			@Override
-	    	public boolean getAsDeamon() {
-	    		return asDaemon;
-	    	}
-
-			@Override
-	    	public User getCurrentUser() {
-	    		return asDaemon ? userService.getDaemonAccountUser() : userService.getCurrentUser();
-	    	}
-	    }
-
-	    /**
-	     * Private constructor
-	     */
-	    private TestContextFactory() {}
-
-	    public static CallingContext getCallingContext() {
-	    	return new CallingContextImpl();
-	    }
+	public static CallingContext getCallingContext() {
+		return new CallingContextImpl();
+	}
 
 }
